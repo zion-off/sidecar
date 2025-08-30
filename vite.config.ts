e79659e42e2b70997332cadc049b312ac1eebad8
zion <zion@nyu.edu>
2025-08-30 15:18:25 +0600

    fix vite config issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    root: "src",
-    publicDir: "public",
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
+  plugins: [react()],
+  root: 'src',
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src')
+    }
+  },
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        content: './src/content.ts',
+        background: './src/background.ts',
+        index: './src/index.html'
+      },
+      output: {
+        entryFileNames: (assetInfo) => {
+          return assetInfo.name === 'content' || assetInfo.name === 'background' ? '[name].js' : 'assets/[name].js';
         },
-    },
-    build: {
-        outDir: "../dist",
-        rollupOptions: {
-            input: {
-                content: "./src/content.ts",
-                background: "./src/background.ts",
-                index: "./src/index.html",
-            },
-            output: {
-                entryFileNames: (assetInfo) => {
-                    return assetInfo.name === "content" ||
-                        assetInfo.name === "background"
-                        ? "[name].js"
-                        : "assets/[name].js";
-                },
-                assetFileNames: "assets/[name][extname]",
-            },
-        },
-    },
-    base: "./",
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  },
+  base: './'
 });
