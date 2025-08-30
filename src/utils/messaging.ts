7f0329a2fbca4435fe49608dc543b501078021cf
zion <zion@nyu.edu>
2025-08-30 18:43:30 +0600

    clean up boilerplate

---
+++ src/utils/messaging.ts
@@ -0,0 +1,33 @@
+import { MessageBubble } from '@/types/chat';
+import { PageData } from '@/types/editor';
+
+export const postMessageToParent = (message: object) => {
+  window.parent.postMessage(message, '*');
+};
+
+export const sampleSuggestedCode = `def twoSum(nums, target):
+    """
+    Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target.
+    """
+    num_to_index = {} # More descriptive variable name
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[num] = index
+    # No explicit return is needed if a solution is guaranteed.`;
+
+export const sampleMessages = (pageData: PageData | null): MessageBubble[] => {
+  return [
+    {
+      message: `Title: ${pageData?.title}
+      Description: ${pageData?.description.slice(0, 10)}
+      Current editor content: ${pageData?.editorContent.slice(0, 10)}
+      Current editor language: ${pageData?.language}`,
+      role: 'assistant'
+    },
+    { message: 'Hello, how can I assist you today?', role: 'assistant' },
+    { message: 'I need help with my code.', role: 'user' }
+  ];
+};
