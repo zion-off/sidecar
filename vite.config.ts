5e8bb4b4ae8dc3c910049fd80ab5c18d302f9989
zion <zion@nyu.edu>
2025-08-30 15:56:23 +0600

    setup hot reload
    closes issue #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,47 @@
 import react from '@vitejs/plugin-react';
+import hotReloadExtension from 'hot-reload-extension-vite';
 import { fileURLToPath } from 'node:url';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: 'src',
-  publicDir: 'public',
-  resolve: {
-    alias: {
-      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src')
-    }
-  },
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: {
-        content: './src/content.ts',
-        background: './src/background.ts',
-        index: './src/index.html'
-      },
-      output: {
-        entryFileNames: (assetInfo) => {
-          return assetInfo.name === 'content' || assetInfo.name === 'background' ? '[name].js' : 'assets/[name].js';
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      hotReloadExtension({
+        log: true,
+        backgroundPath: 'src/background.ts'
+      })
+    ].filter(Boolean),
+
+    root: 'src',
+    publicDir: 'public',
+    base: './',
+
+    resolve: {
+      alias: {
+        '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src')
+      }
+    },
+
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          content: './src/content.ts',
+          background: './src/background.ts',
+          index: './src/index.html'
         },
-        assetFileNames: 'assets/[name][extname]'
+        output: {
+          entryFileNames: (assetInfo) => {
+            // Keep background and content scripts at root level
+            return ['content', 'background'].includes(assetInfo.name as string) ? '[name].js' : 'assets/[name].js';
+          },
+          assetFileNames: 'assets/[name][extname]'
+        }
       }
     }
-  },
-  base: './'
+  };
 });
