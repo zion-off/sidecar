40a88390dc7da864baf6c7eef485fe7e34f4ee48
zion <zion@nyu.edu>
2025-09-01 03:14:19 +0600

    fix syntax highlighting

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import 'highlight.js/styles/github-dark.css';
+import 'highlight.js/styles/base16/onedark.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
