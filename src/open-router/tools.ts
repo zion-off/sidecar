3f279408e4d10a6766ad9e48f339a06eefc2ada2
zion <zion@nyu.edu>
2025-09-01 20:39:41 +0600

    add default tools

---
+++ src/open-router/tools.ts
@@ -0,0 +1,21 @@
+import { Tool } from '@/types/open-router';
+
+export const defaultTools: Tool[] = [
+  {
+    type: 'function',
+    function: {
+      name: 'suggest_code',
+      description: 'Suggest code snippets based on the provided context',
+      parameters: {
+        type: 'object',
+        properties: {
+          suggestion: {
+            type: 'string',
+            description: "The code that will replace the user's code"
+          }
+        },
+        required: ['suggestion']
+      }
+    }
+  }
+];
