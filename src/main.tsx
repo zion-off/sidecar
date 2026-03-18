c0dad7b7ceee5b758498c0531191da0dbeda0be7
zion <zion@nyu.edu>
2026-03-19 01:04:27 +0600

    feat(ui): change highlightjs theme to vs2015

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import 'highlight.js/styles/base16/onedark.css';
+import 'highlight.js/styles/vs2015.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
